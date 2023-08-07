309b276cae895e42f770612edfc56bb0b4fde9c6
Jae Kim <9028364@gmail.com>
2023-08-08 01:43:40 +0900

    feat: Section 2

---
+++ section2/metacharacter.js
@@ -0,0 +1,16 @@
+// Wildcard
+const txt = "how it that so hot?";
+const regex1 = /h.t/g;
+
+// Escape
+const txt2 = "This could be the final word.";
+const regex2 = /d\./g;
+
+// Control
+// \t tab
+// \v vertical
+// \n newline
+// \r carriage return
+
+const txt2 = "This could be the final word.";
+const regex2 = /d\./g;
